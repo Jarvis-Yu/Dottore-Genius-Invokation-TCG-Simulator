453d2428bbb16c377cb1c3a3012023ee3d5e374c
JarvisYu <JarvisYu_Pro@outlook.com>
2023-05-30 20:43:13 +0100

    renames NumberedStatus to DurationStatus

--- dgisim/src/status/status.py
+++ dgisim/src/status/status.py
@@ -62,22 +62,22 @@ class CombatStatus(Status):
     pass
 
 
-class NumberedStatus(Status):
+class DurationStatus(Status):
     """
-    This class has a num which acts as a counter
+    This class has a duration which acts as a counter
     """
 
-    def __init__(self, num: int = 0) -> None:
-        self._num = num
+    def __init__(self, duration: int = 0) -> None:
+        self._duration = duration
 
-    def num(self) -> int:
-        return self._num
+    def duration(self) -> int:
+        return self._duration
 
     def __str__(self) -> str:
-        return super().__str__() + f"({self._num})"
+        return super().__str__() + f"({self._duration})"
 
 
-class NumberedAutoDestroyStatus(NumberedStatus):
+class DurationAutoDestroyStatus(DurationStatus):
     """
     This class destroys the status if the new counter <= 0;
     To set how the status react to signal, override the method `actual_react_to_signal`
@@ -87,22 +87,22 @@ class NumberedAutoDestroyStatus(NumberedStatus):
             self, source: eft.StaticTarget, signal: eft.TriggeringSignal
     ) -> tuple[eft.Effect, ...]:
         """
-        This method adds effects destorying or updating the current status based on the new number
+        This method adds effects destorying or updating the current status based on the new duration
         provided
         """
-        num, es = self.actual_react_to_signal(source, signal)
-        if num <= 0:
+        duration, es = self.actual_react_to_signal(source, signal)
+        if duration <= 0:
             es += (
                 eft.RemoveCharacterStatusEffect(
                     source,
                     type(self),
                 ),
             )
-        elif num != self.num():
+        elif duration != self.duration():
             es += (
                 eft.UpdateStatusEffect(
                     source,
-                    MushroomPizzaStatus(num),
+                    MushroomPizzaStatus(duration),
                 ),
             )
         return es
@@ -113,8 +113,8 @@ class NumberedAutoDestroyStatus(NumberedStatus):
         """
         This method should provide what this status actually does
         It is supposed to be overriden
-        Return value is (new_num, effects):
-        - The new_num is the new num the reacted status should have
+        Return value is (new_duration, effects):
+        - The new_duration is the new duration the reacted status should have
         - The effects are just the effects generated according to the signal
         """
         raise Exception("Not overriden")
@@ -132,21 +132,21 @@ class SatiatedStatus(CharacterStatus):
         return ()
 
 
-class MushroomPizzaStatus(CharacterStatus, NumberedAutoDestroyStatus):
-    def __init__(self, num: int = 2) -> None:
-        super().__init__(num)
+class MushroomPizzaStatus(CharacterStatus, DurationAutoDestroyStatus):
+    def __init__(self, duration: int = 2) -> None:
+        super().__init__(duration)
 
     @override
     def actual_react_to_signal(
             self, source: eft.StaticTarget, signal: eft.TriggeringSignal
     ) -> tuple[int, tuple[eft.Effect, ...]]:
         if signal is eft.TriggeringSignal.END_ROUND_CHECK_OUT:
-            new_num = self.num() - 1
+            new_duration = self.duration() - 1
             es = (
                 eft.RecoverHPEffect(
                     source,
                     1,
                 ),
             )
-            return new_num, es
-        return (self.num(), ())
+            return new_duration, es
+        return (self.duration(), ())
