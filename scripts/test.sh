03fe7c995cb13b96e44c19246d7a0c6f61adc0dd
JarvisYu <JarvisYu_Pro@outlook.com>
2023-06-08 23:10:10 +0100

    adds coverage to test unittest coverage

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-./venv/bin/python -m unittest
-./venv/bin/mypy dgisim/ --check-untyped-defs
+#./venv/bin/python -m unittest
+echo -ne "#################### Unittest ####################\n" && \
+./venv/bin/coverage run -m unittest && \
+echo -ne "\n#################### Mypy ####################\n" && \
+./venv/bin/mypy dgisim/ --check-untyped-defs && \
+echo -ne "\n#################### Coverage ####################\n" && \
+./venv/bin/coverage report
