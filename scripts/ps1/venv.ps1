76a276b1f09de96c6be0c3dffa74d2c35dd363ba
nandofromthebando <87321214+nandofromthebando@users.noreply.github.com>
2023-07-04 08:16:31 -0700

    issue #17 cont

--- scripts/ps1/venv.ps1
+++ scripts/ps1/venv.ps1
@@ -1,42 +1,6 @@
-$venvPath = ".\venv"
-$activateScript = Join-Path -Path $venvPath -ChildPath "Scripts\Activate.ps1"
 
-if (Test-Path -Path $venvPath) {
-    Write-Host "venv is already setup!"
-    Write-Host "Press any key to continue..."
-    $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | Out-Null
-} else {
-    Write-Host "Setting up venv, this may take a minute..."
-
-    # Create virtual environment
-    try {
-        python -m venv .\venv
-    } catch {
-        Write-Host "Could not create virtual environment"
-        exit 1
-    }
-
-    # Activate virtual environment
-    try {
-        . $activateScript
-    } catch {
-        Write-Host "Could not activate virtual environment"
-        exit 1
-    }
-
-    # Install requirements
-    try {
-        pip install -r requirements.txt
-    } catch {
-        Write-Host "Could not install requirements"
-        deactivate
-        exit 1
-    }
-    
-    # Finish up
-    deactivate
-    Write-Host "venv setup complete!"
+if (-not (Test-Path "venv")){
+  python -m venv venv
 }
-
-Write-Host "Press any key to continue..."
-$host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | Out-Null
+& .\venv\bin\activate
+& .\venv\bin\python.exe -m pip install -r requirements.txt
