dfe4d71aa19bd8be3b338b68779097b22b45f8e6
JarvisYu <JarvisYu_Pro@outlook.com>
2023-10-20 20:03:35 +0100

    adjusts project import hierarchy as a package

---
+++ src/package/__init__.py
@@ -0,0 +1,55 @@
+from ._core import agents
+from ._core.cli import *
+from ._core.deck import *
+from ._core.dice import *
+from ._core.element import *
+from ._core.event import *
+from ._core.game_state_machine import *
+from ._core.mode import *
+from ._core.player_agent import *
+
+from ._core.action.action import *
+from ._core.action.action_generator import *
+from ._core.action.action_generator_generator import *
+
+from ._core.card import card
+from ._core.card.card import Card
+from ._core.card.cards import *
+from ._core.card.cards_set import *
+
+from ._core.character import character as char
+from ._core.character.character import Character
+from ._core.character.characters import *
+from ._core.character.characters_set import *
+from ._core.character.enums import *
+
+from ._core.effect import effect
+from ._core.effect.effect import Effect
+from ._core.effect.effect_stack import *
+from ._core.effect.effects_template import *
+from ._core.effect.enums import *
+from ._core.effect.structs import *
+
+from ._core.helper.hashable_dict import *
+
+from ._core.phase.phase import *
+from ._core.phase.default import *
+from ._core.phase import all_omni
+
+from ._core.state.enums import *
+from ._core.state.game_state import *
+from ._core.state.player_state import *
+
+from ._core.status import status
+from ._core.status.enums import *
+from ._core.status.status_processing import *
+from ._core.status.status import Status
+from ._core.status.statuses import *
+
+from ._core.summon import summon
+from ._core.summon.summon import Summon
+from ._core.summon.summons import *
+
+from ._core.support import support
+from ._core.support.support import Support
+from ._core.support.supports import *
