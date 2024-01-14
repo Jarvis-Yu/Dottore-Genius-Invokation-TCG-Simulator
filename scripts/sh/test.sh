a5197a6e5902dccf2ace9c682cbd73bbba607219
JarvisYu <JarvisYu_Pro@outlook.com>
2024-01-14 14:22:13 +0000

    enables more encoding tests in GitHub Actions

--- scripts/sh/test.sh
+++ scripts/sh/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export RNG_PLAYS=5
+export ENCODING_RUNS=2
 export SHOW_PROGRESS=1
 echo -ne "#################### Unittest ####################\n" && \
 ./venv/bin/coverage run -m unittest discover src/tests/ && \
--- src/tests/test_encodnig.py
+++ src/tests/test_encodnig.py
@@ -7,13 +7,48 @@ from src.dgisim.state.game_state import GameState
 
 class TestEncoding(unittest.TestCase):
     def test_encoding(self):
+        import os, sys, time
+        optional_repeats = os.getenv("ENCODING_RUNS")
+        optional_show_progress = os.getenv("SHOW_PROGRESS")
+        repeats: int
+        show_progress: bool
+        try:
+            repeats = int(optional_repeats) if optional_repeats is not None else 5
+            show_progress = (
+                int(optional_show_progress) != 0
+                if optional_show_progress is not None
+                else False
+            )
+        except:
+            repeats = 5
+            show_progress = False
+
         game_states: list[GameState] = []
-        for _ in range(2):
+        prev_progress = ""
+        for i in range(repeats):
+            if show_progress:
+                print(end='\b' * len(prev_progress))
+                prev_progress = f"[{i}/{repeats}]"
+                print(end=prev_progress)
+                sys.stdout.flush()
             gsm = GameStateMachine(GameState.from_default(), RandomAgent(), RandomAgent())
             gsm.run()
             game_states.extend(gsm.get_history())
-        encodings = set([
-            len(game_state.encoding(encoding_plan))
-            for game_state in game_states
-        ])
-        self.assertEqual(len(encodings), 1)
+
+        if not show_progress:
+            encodings = set([
+                len(game_state.encoding(encoding_plan))
+                for game_state in game_states
+            ])
+            self.assertEqual(len(encodings), 1, encodings)
+        else:
+            encodings = set()
+            for i, game_state in enumerate(game_states):
+                print(end='\b' * len(prev_progress))
+                prev_progress = f"[{i}/{len(game_states)}]"
+                print(end=prev_progress)
+                sys.stdout.flush()
+                encodings.add(len(game_state.encoding(encoding_plan)))
+            print(end='\b' * len(prev_progress))
+            sys.stdout.flush()
+            self.assertEqual(len(encodings), 1, encodings)
