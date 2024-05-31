9e6a2409d414107ac3a571e87519e1dbe5ff7edb
Tomorrowdawn <tomorrowdawn@foxmail.com>
2024-05-31 23:01:31 +0800

    update docstring of LazyEncodingPlan

--- src/dgisim/encoding/encoding_plan.py
+++ src/dgisim/encoding/encoding_plan.py
@@ -270,7 +270,7 @@ class LazyEncodingPlan(EncodingPlan):
     """
     def encode(self, game_state: "GameState", perspective:Pid):
         """
-        :returns: None.
+        :returns: [].
         """
         return []
 
